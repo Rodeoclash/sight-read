27a02bc4db6efce6485cb6b027102e9a3fa2dbf4
Samuel Richardson <sam@richardson.co.nz>
2024-11-23 21:33:05 +1100

    Add some analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -11,5 +12,6 @@ if (rootEl === null) {
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
